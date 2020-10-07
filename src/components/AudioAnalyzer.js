453eccd9cf6f5bd0c05be978c17167de87b1de6f
TNoga0 <t.noga0@gmail.com>
2020-10-07 21:32:22 +0200

    Add basic FFT audio data gathering and callback to repeat

--- src/components/AudioAnalyzer.js
+++ src/components/AudioAnalyzer.js
@@ -6,6 +6,8 @@ function AudioAnalyzer() {
   // WebAudio API's contexts, audio elements etc.
   // vars had to be used instead of consts
   const audioContext = new AudioContext();
+  var audioAnalyser;
+  var dataArray;
   var [audioElement, setAudioElement] = useState(null);
   var [audioTrack, setAudioTrack] = useState(null);
 
@@ -16,13 +18,26 @@ function AudioAnalyzer() {
   useEffect(() => {
     audioElement = document.querySelector('audio');
     audioTrack = audioContext.createMediaElementSource(audioElement);
+    audioAnalyser = audioContext.createAnalyser();
+    audioAnalyser.fftSize = 256;
+    dataArray = new Float32Array(audioAnalyser.frequencyBinCount);
     audioTrack.connect(audioContext.destination);
+    audioTrack.connect(audioAnalyser);
   }, [audioElement, audioTrack]);
+
+  // Gather FFT audio data and redraw animation until music is playing
+  function draw() {
+    audioAnalyser.getFloatFrequencyData(dataArray);
+    if (musicPlaying) {
+      window.requestAnimationFrame(draw);
+    }
+  }
 
   // A function to play/pause music (audioElement) using a flag
   function playMusic() {
     if (!musicPlaying) {
       audioElement.play();
+      window.requestAnimationFrame(draw);
     }
     else {
       audioElement.pause();
@@ -39,4 +54,4 @@ function AudioAnalyzer() {
   );
 }
 
-export default AudioAnalyzer
+export default AudioAnalyzer;
