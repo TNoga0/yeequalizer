90937a2abb9878f174a243615baa8cf80a4eff2f
TNoga0 <t.noga0@gmail.com>
2020-10-09 23:31:03 +0200

    Add basic frequencies drawing and gain node

--- src/components/AudioAnalyzer.js
+++ src/components/AudioAnalyzer.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState, useRef} from "react";
+import Sketch from "react-p5";
 import "../App.css";
 
 function AudioAnalyzer() {
@@ -6,12 +7,17 @@ function AudioAnalyzer() {
   // WebAudio API's contexts, audio elements etc.
   // vars had to be used instead of consts
   const audioContext = new AudioContext();
-  var audioAnalyser;
+  var audioAnalyser, audioGain;
   var dataArray;
   var [audioElement, setAudioElement] = useState(null);
   var [audioTrack, setAudioTrack] = useState(null);
 
   var musicPlaying = false;
+
+  // values used to map and draw rectangles
+  var outMin = 0;
+  var outMax = 512;
+  var inMin, inMax;
 
   // TODO provide ability to upload custom songs
   // Effect at render to set all the necessary WebAudio elements
@@ -19,17 +25,20 @@ function AudioAnalyzer() {
     audioElement = document.querySelector('audio');
     audioTrack = audioContext.createMediaElementSource(audioElement);
     audioAnalyser = audioContext.createAnalyser();
+    audioGain = audioContext.createGain();
     audioAnalyser.fftSize = 256;
-    dataArray = new Float32Array(audioAnalyser.frequencyBinCount);
-    audioTrack.connect(audioContext.destination);
+    dataArray = new Uint8Array(audioAnalyser.frequencyBinCount);
+    audioTrack.connect(audioGain).connect(audioContext.destination);
+    audioGain.gain.value = 0.3;
     audioTrack.connect(audioAnalyser);
   }, [audioElement, audioTrack]);
 
   // Gather FFT audio data and redraw animation until music is playing
-  function draw() {
-    audioAnalyser.getFloatFrequencyData(dataArray);
+  // Bass is from 60 to 250 Hz and that's
+  function drawFrequencyData() {
+    audioAnalyser.getByteFrequencyData(dataArray);
     if (musicPlaying) {
-      window.requestAnimationFrame(draw);
+      window.requestAnimationFrame(drawFrequencyData);
     }
   }
 
@@ -37,7 +46,7 @@ function AudioAnalyzer() {
   function playMusic() {
     if (!musicPlaying) {
       audioElement.play();
-      window.requestAnimationFrame(draw);
+      window.requestAnimationFrame(drawFrequencyData);
     }
     else {
       audioElement.pause();
@@ -45,11 +54,27 @@ function AudioAnalyzer() {
     musicPlaying = !musicPlaying;
   }
 
+  // p5.js methods
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(512, 512).parent(canvasParentRef);
+  };
+
+  var i;
+  const draw = (p5) => {
+    p5.background(255);
+    p5.fill(p5.color(0));
+    for (i = 0; i < dataArray.length; i++) {
+      p5.rect((4 * i), 512, 4, -2 * dataArray[i]);
+    }
+  };
+
   return(
     <div className="AudioAnalyzer">
       <h2>HOLA COMPANEROS</h2>
       <audio src="static/tokyodrift.mp3"/>
       <button className="PlayButton" onClick={playMusic}>PLAY THE EUROBEAT</button>
+      <br/><br/>
+      <Sketch setup={setup} draw={draw}/>
     </div>
   );
 }
